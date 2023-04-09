9eeec518d3a4820be9d48f363555ef8333faf8de
Mrered Cio <mrered@Mrered-MBA.local>
2023-04-09 15:15:48 +0800

    先传上去再说

---
+++ CloudflareSTCustom.sh
@@ -0,0 +1,33 @@
+#!/bin/zsh
+
+# 打开 URL Scheme
+open "clash://quit"
+
+# 等待 Clash for Windows 关闭
+while pgrep -x "Clash for Windows" > /dev/null; do
+  sleep 1
+done
+
+# 弹出开始提示
+osascript -e 'display notification "开始优选Cloudflare CDN IPV6" with title "代理已关闭"'
+
+# 等待2秒钟
+sleep 1
+
+# 执行 CloudflareST
+/opt/homebrew/Cellar/cfstarm64/2.2.2/bin/CloudflareST -f /opt/homebrew/Cellar/cfstarm64/2.2.2/bin/ipv6.txt -o ~/.CloudflareSTResult/result.csv -n 1000 -dn 10 -tll 30 -dt 13 -tl 280 -sl 15 -p 5
+
+# 等待2秒钟
+sleep 1
+
+# 打开 Clash for Windows
+open "/Applications/Clash for Windows.app"
+
+# 等待2秒钟
+sleep 1
+
+# 弹出提示信息并退出终端窗口
+osascript -e 'display notification "优选完成" with title "代理已打开"'
+
+# 关闭终端窗口
+osascript -e 'tell application "Terminal" to close first window' & exit
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# cfstc
-自用
+# CloudflareSpeedTestCustom
+
+## 这是什么
+
+这是我自己用的一个小玩意儿。
